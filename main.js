2a33adff861cddd55517dac05b00eb936d9ae3fd
GTM-Winter <dimastriana03@gmail.com>
2022-07-06 16:48:51 +0700

    clean code

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-// Nilai default jika tiak meng klik dark mode
-  $.getJSON('data/redvelvet.json', function (data) {
-    // Ini ambil data yg grup
-    let grup = data.Grup;
-    $.each(grup, function(i,data){
-      $('#grup').append(`
-       <img src="./img/light/`+ data.Gambar +`" alt="">
-       <p class="text-center text-dark mt-2">
-          Irene | Seulgi | Wendy | Joy | Yeri
-        </p>
-      `)
-    });
-    //ini ambil data member di json
-    let member = data.RV;
-    $.each(member, function(i, data) {
-      $('#daftar-member').append('<div class="cont_m profile" data-aos="fade-up" data-aos-delay="100"><div class="pf_member center"><div class="pf_img"><img class="rv__member" src="img/light/'+ data.Gambar +'" alt="Irene" /></div><div class="pf_box_m"><div class="pf_txt"><div class="pf_name coend-marker" data-aos="fade-right" data-aos-delay="200"> '+ data.Nama +'</div></div><div class="pf_txt_cont" data-aos="fade-right" data-aos-delay="300"><p><span>Nama:'+ data.Nama_Asli+'</span></p><p><span>Tanggal Lahir:'+ data.Lahir +'</span></p></div></div></div></div></div>')
-    });
-  });
-  
- // function ketika light mode
+// function ketika light mode
 function light() {
 // function untuk ambil data json
   $.getJSON('data/redvelvet.json', function (data) {
@@ -63,6 +44,12 @@ function dark() {
 }
 
 
+// Nilai default jika tiak meng klik dark mode
+light();
+
+
+
+
 // dark mode
 $('.checkbox').on('click' ,function(){
  var hasil=document.body.classList.toggle('dark');
@@ -77,8 +64,9 @@ $('.checkbox').on('click' ,function(){
  }
 });
 
+
 // navbar active gitulah
 $('.nav-link').on('click', function() {
   $('.nav-link').removeClass('active');
-  $('this').addClass('active');
+  $(this).addClass('active');
 });
